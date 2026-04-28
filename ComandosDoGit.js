84058c548e9d8f5fa0b558347b1835f0ac2889fa
Gabriel Braga Ravanelli <gabrielravanelli1@gmail.com>
2026-04-28 11:35:50 -0300

    |Comandos básicos do git

---
+++ ComandosDoGit.js
@@ -0,0 +1,14 @@
+// Para entrar em uma pasta, utiliza o comando "cd" + o nome da pasta.
+// Para sair de um pasta, utiliza-se o comando "cd ..".
+// Para criar uma pasta, utiliza-se o comando "mkdir" + o nome da pasta.
+// Para criar um arquivo, utiliza-se o comando "touch" + o nome do arquivo.
+
+// Para criar um repositório, deve ser utilizado o comando "git init" + o nome do repositorio.
+// o Git status, mostra os arquivos que foram modificados  e arquivos que estão prontos para serem commitados.
+// Para adicionar as alterações no repositoriao virtual, utiliza-se o comando "git add .".
+// Para confirmar as mudanças, precisa usar o comando "git commit -m "Aqui vai a mensagem do commit".
+
+// Caso eu queira que o git ignore um arquivo, eu preciso criar um arquivo chamado .gitignore e tudo que for escrito dentro desse arquivo, o git irá ignorar.
+// git diff serve para mostrar as mudanças que foram feitas em um arquivo, mas que ainda não foram adicionadas ao repositório virtual.
+// Agora, se as mundanças já foram comitadas, o comando "git log" irá mostrar o historico de commit.
+// Ainda no git log, se acrescentar o -p, ele irá mostrar as mudanças que foram feitas em cada commit.
