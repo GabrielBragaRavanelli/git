4896c1c02d6bf354704ee1ec2b03aa2ae148af86
Gabriel Braga Ravanelli <gabrielravanelli1@gmail.com>
2026-04-29 11:56:08 -0300

    Finalizando o primeiro curso de git

--- ComandosDoGit.js
+++ ComandosDoGit.js
@@ -16,3 +16,18 @@
 // o git commit --amend, serve para modificar o ultimo commit, seja para alterar a mensagem do commit ou para adicionar arquivos que foram esquecidos no ultimo commit.
 // caso eu tenha dado git add e enviado os arquivos para o repositorio virutal, eu posso retirar esses arquivos por meio do "git reset HEADA" e o git irá retirar os arquivos do repositorio virtual, mas eles ainda estarão presentes no meu computador, ou seja, eles não serão deletados.
 // caso você tenha feito muitas alterações e queira reverter todas as mudanças para o arquivo original. Apenas utilize o comando "git checkout -- nome do arquivo" e o git irá reverter as mudanças para o arquivo original, ou seja, ele irá retirar todas as mudanças feitas no arquivo e irá deixar o arquivo como estava antes das mudanças.
+
+// tem o comando git tag, que serve para marcar um commit específico, ou seja, ele serve para criar uma tag em um commit específico, para facilitar a identificação desse commit no futuro.
+// Para criar uma tag, você tem que colocar "git tag -a" + o nome da tag + o hash do commit + -m "a mensagem da tag".
+// git show + o nome da tag, irá mostrar as informações da tag, como o hash do commit, a mensagem da tag e a data da tag, quem criou a tag e etc.
+// Para deletar uma tag, basta utilizar git tag -d + o nome da tag
+// git branch + o nome da branch 
+// agora git checkout + o nome da branch, para entrar na branch criada
+// se você apenas colocar git checkout -b + o nome da branch, ele irá criar a branch e já irá entrar nela.
+// git checkou master, para voltar para a branch master
+// git merge + o nome da branch, para fazer o merge da branch com a branch atual, ou seja, ele irá juntar as mudanças da branch com a branch atual.
+// para deletar git branch -d + o nome da branch, para deletar a branch, mas para isso a branch precisa estar mesclada com a branch master, ou seja, as mudanças da branch precisam estar presentes na branch master, caso contrário, o git irá mostrar um erro dizendo que a branch não pode ser deletada porque ela não está mesclada com a branch master.
+// git clone + o código do repositório, para clonar um repositório.
+// git pull, para puxar as mudanças do repositório remoto para o repositório local, ou seja, ele irá atualizar o repositório local com as mudanças do repositório remoto.
+// git push, para enviar as mudanças do repositório local para o repositório remoto, ou seja, ele irá atualizar o repositório remoto com as mudanças do repositório local.
+// git push origin master, faz com que as mundaãs no arquivo origin vá para o arquivo master.
