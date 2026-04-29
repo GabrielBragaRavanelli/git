1e408044dabcc0d93f3c2c870ab62441a2d22289
Gabriel Braga Ravanelli <gabrielravanelli1@gmail.com>
2026-04-28 23:35:05 -0300

    Aula 04

--- ComandosDoGit.js
+++ ComandosDoGit.js
@@ -12,3 +12,7 @@
 // git diff serve para mostrar as mudanças que foram feitas em um arquivo, mas que ainda não foram adicionadas ao repositório virtual.
 // Agora, se as mundanças já foram comitadas, o comando "git log" irá mostrar o historico de commit.
 // Ainda no git log, se acrescentar o -p, ele irá mostrar as mudanças que foram feitas em cada commit.
+// o git log --oneline, irá mostrar o historico de commit, mas de forma resumida, mostrando apenas o hash do commit e a mensagem do commit.
+// o git commit --amend, serve para modificar o ultimo commit, seja para alterar a mensagem do commit ou para adicionar arquivos que foram esquecidos no ultimo commit.
+// caso eu tenha dado git add e enviado os arquivos para o repositorio virutal, eu posso retirar esses arquivos por meio do "git reset HEADA" e o git irá retirar os arquivos do repositorio virtual, mas eles ainda estarão presentes no meu computador, ou seja, eles não serão deletados.
+// caso você tenha feito muitas alterações e queira reverter todas as mudanças para o arquivo original. Apenas utilize o comando "git checkout -- nome do arquivo" e o git irá reverter as mudanças para o arquivo original, ou seja, ele irá retirar todas as mudanças feitas no arquivo e irá deixar o arquivo como estava antes das mudanças.
